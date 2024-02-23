037f5a9b7e076ca103c249da8208798fff3a4354
MRMutahir <mutahirkareem820@gmail.com>
2024-02-23 11:03:09 +0500

    Missing or insufficient permissions.
     issue  solve
     ans :      allow read, write: if request.time > timestamp.date(2023, 12, 13);

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,25 +6,41 @@ import Recommends from "./Recommends";
 import Trending from "./Trending";
 import Viewers from "./Viewers";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setMovies } from "../features/movie/movieSlice.js";
-import { selectUserName } from "../features/User/UserSlice.js";
-import {db}  from "../FireBase.js"
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+// import { useDispatch, useSelector } from "react-redux";
+// import { setMovies } from "../features/movie/movieSlice.js";
+// import { selectUserName } from "../features/User/UserSlice.js";
+// import { database } from "../FireBase.js";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { db } from "../FireBase";
 
 function Home() {
-  const dispatch = useDispatch();
-  const userName = useSelector(selectUserName);
+  useEffect(() => {
+    console.log("useeffect  run");
+    async function getdata() {
+      console.log("function");
+      const querySnapshot = await getDocs(collection(db, "movie"));
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+      });
+      // const db = getDatabase();
+      // const starCountRef = ref(db, "movie/");
+      // onValue(starCountRef, (snapshot) => {
+      //   const data = snapshot.val();
+      //   console.log("data", data);
+      // });
+    }
+    getdata();
+  }, []);
   // let linkSnapshotListener;  // Declare 'linkSnapshotListener'
   // let isRenderingLinks = false;  // Declare 'isRenderingLinks'
 
+  // useEffect(() => {
+  // if (linkSnapshotListener) {
+  //   linkSnapshotListener();
+  // }
 
-
-
-   // useEffect(() => {
-    // if (linkSnapshotListener) {
-    //   linkSnapshotListener();
-    // }
-  
   //   let collectionName = `movie`;
   //   linkSnapshotListener = db.collection(`${collectionName}`)
   //     .orderBy("timestamp", "desc")
@@ -35,10 +51,10 @@ function Home() {
   //         console.log(data);
   //         // movies.push({ id: doc.id, ...data });
   //       });
-  
+
   //       // Dispatch an action or update local state with the movies array
   //       dispatch(setMovies(movies));
-  
+
   //       isRenderingLinks = false;
   //     }, function (error) {
   //       console.log("Error getting documents: ", error);
@@ -46,112 +62,108 @@ function Home() {
   //     });
   // }, [dispatch]);
 
+  // const unsubscribe = db.collection("movies").onSnapshot((snapshot) => {
+  //   const recommends = [];
+  //   const newDisneys = [];
+  //   const originals = [];
+  //   const trending = [];
 
+  //   snapshot.docs.forEach((doc) => {
+  //     switch (doc.data().type) {
+  //       case "recommend":
+  //         recommends.push({ id: doc.id, ...doc.data() });
+  //         break;
 
+  //       case "new":
+  //         newDisneys.push({ id: doc.id, ...doc.data() });
+  //         break;
 
-// const unsubscribe = db.collection("movies").onSnapshot((snapshot) => {
-//   const recommends = [];
-//   const newDisneys = [];
-//   const originals = [];
-//   const trending = [];
+  //       case "original":
+  //         originals.push({ id: doc.id, ...doc.data() });
+  //         break;
 
-//   snapshot.docs.forEach((doc) => {
-//     switch (doc.data().type) {
-//       case "recommend":
-//         recommends.push({ id: doc.id, ...doc.data() });
-//         break;
+  //       case "trending":
+  //         trending.push({ id: doc.id, ...doc.data() });
+  //         break;
+  //     }
+  //   });
 
-//       case "new":
-//         newDisneys.push({ id: doc.id, ...doc.data() });
-//         break;
+  //   dispatch(
+  //     setMovies({
+  //       recommend: recommends,
+  //       newDisney: newDisneys,
+  //       original: originals,
+  //       trending: trending,
+  //     })
+  //   );
+  // });
 
-//       case "original":
-//         originals.push({ id: doc.id, ...doc.data() });
-//         break;
+  // // Cleanup subscription on unmount
+  // return () => unsubscribe();
 
-//       case "trending":
-//         trending.push({ id: doc.id, ...doc.data() });
-//         break;
-//     }
-//   });
+  // console.log(db._databaseId.projectId, ">>>>>>>>collectiondb");
+  // let id = db._databaseId.projectId;
+  // console.log(id);
+  // const dispatch = useDispatch();
+  // const username = useSelector(selectUserName);
+  // let recommends = [];
+  // let newDisney = [];
+  // let originals = [];
+  // let trending = [];
 
-//   dispatch(
-//     setMovies({
-//       recommend: recommends,
-//       newDisney: newDisneys,
-//       original: originals,
-//       trending: trending,
-//     })
-//   );
-// });
+  // useEffect(() => {
+  //   async function faq() {
+  //     const querySnapshot = await getDocs(collection(db, "movie"));
+  //     querySnapshot.forEach((doc) => {
+  //       // doc.data() is never undefined for query doc snapshots
+  //       // console.log(doc.id, "=> ", doc.data().type);
+  //       let firebaseData = doc.data();
+  //       // console.log(firebaseData.type);
+  //       switch (firebaseData.type) {
+  //         case "recommend":
+  //           recommends = [...recommends, { id: doc.id, ...firebaseData }];
+  //           // console.log(recommends);
 
-// // Cleanup subscription on unmount
-// return () => unsubscribe();
+  //           break;
+  //         case "newDisney":
+  //           newDisney = [...newDisney, { id: doc.id, ...firebaseData }];
+  //           // console.log(newDisney);
 
+  //           break;
+  //         case "original":
+  //           originals = [...originals, { id: doc.id, ...firebaseData }];
+  //           // console.log(originals);
 
-// console.log(db._databaseId.projectId, ">>>>>>>>collectiondb");
-// let id = db._databaseId.projectId;
-// console.log(id);
-// const dispatch = useDispatch();
-// const username = useSelector(selectUserName);
-// let recommends = [];
-// let newDisney = [];
-// let originals = [];
-// let trending = [];
+  //           break;
+  //         case "trending":
+  //           trending = [...trending, { id: doc.id, ...firebaseData }];
+  //           // console.log(trending);
 
-// useEffect(() => {
-//   async function faq() {
-//     const querySnapshot = await getDocs(collection(db, "movie"));
-//     querySnapshot.forEach((doc) => {
-//       // doc.data() is never undefined for query doc snapshots
-//       // console.log(doc.id, "=> ", doc.data().type);
-//       let firebaseData = doc.data();
-//       // console.log(firebaseData.type);
-//       switch (firebaseData.type) {
-//         case "recommend":
-//           recommends = [...recommends, { id: doc.id, ...firebaseData }];
-//           // console.log(recommends);
+  //           break;
+  //       }
+  //     });
+  //   }
+  //   faq();
+  //   dispatch(
+  //     setMovies({
+  //       recommend: recommends,
+  //       newDisney: newDisney,
+  //       original: originals,
+  //       trending: trending,
+  //     })
+  //   );
+  // }, [username]);
 
-//           break;
-//         case "newDisney":
-//           newDisney = [...newDisney, { id: doc.id, ...firebaseData }];
-//           // console.log(newDisney);
-
-//           break;
-//         case "original":
-//           originals = [...originals, { id: doc.id, ...firebaseData }];
-//           // console.log(originals);
-
-//           break;
-//         case "trending":
-//           trending = [...trending, { id: doc.id, ...firebaseData }];
-//           // console.log(trending);
-
-//           break;
-//       }
-//     });
-//   }
-//   faq();
-//   dispatch(
-//     setMovies({
-//       recommend: recommends,
-//       newDisney: newDisney,
-//       original: originals,
-//       trending: trending,
-//     })
-//   );
-// }, [username]);
-
-return (
-  <Container>
-    <ImgSlider />
-    <Viewers />
-    <Recommends />
-    <NewDisney />
-    <Originals />
-    <Trending />
-  </Container>
-);
+  return (
+    <Container>
+      <ImgSlider />
+      <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
+    </Container>
+  );
 }
 
 const Container = styled.main`
--- src/FireBase.js
+++ src/FireBase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getDatabase } from "firebase/database";
-
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+// import { getDatabase } from "firebase/database";
 
 // import {  } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyDHZCSKSdGoYNnNyk51rD745c-CxKVAjZI",
   authDomain: "disney-13901.firebaseapp.com",
+  databaseURL: "com.google.firebase:firebase-bom:32.7.2",
   projectId: "disney-13901",
   storageBucket: "disney-13901.appspot.com",
   messagingSenderId: "497421052453",
@@ -18,6 +18,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
-export const database = getDatabase();
+// export const database = getDatabase();
 export default app;
-
